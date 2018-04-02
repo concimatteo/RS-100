6b80ed7a0778a82752591721dde52e36c5df45e6
CanzoniereOnLine <canzoniereonline@gmail.com>
2018-04-02 10:17:44 +0200

    iniziamo!

--- putimage.php
+++ putimage.php
@@ -3,6 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+
 function generateRandomString($length = 32) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $charactersLength = strlen($characters);
