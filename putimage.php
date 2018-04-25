81954f62cb6c6a893786e0ef428fd0cbb77939af
Andrea Conci <Andrea@MacBook-Pro-di-Andrea.local>
2018-04-25 10:37:15 +0200

    no pws in putimage

--- putimage.php
+++ putimage.php
@@ -262,8 +262,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.mailgun.org';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'postmaster@mail.canzoniereonline.it';                 // SMTP username
-    $mail->Password = '575850eb3db71c519afa5ba04756ec54';                           // SMTP password
+    $mail->Username = '';                 // SMTP username
+    $mail->Password = '';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
